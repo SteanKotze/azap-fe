0002e614a2dfbd2ef115912e6458b1f298ad4ca4
Stean Kotze <stean@redbear.studio>
2021-08-22 23:51:49 +0200

    Smol changes.

--- src/components/links/Link.svelte
+++ src/components/links/Link.svelte
@@ -4,10 +4,11 @@
 
   //  --- Props ---
   export let className = ''
+  export let hover = true
   export let textSize = ''
   export let to = undefined
 </script>
 
-<Link {to} class={`cursor-pointer text-gold hover:text-gold-light ${className} ${textSize}`}>
+<Link {to} class={`cursor-pointer text-gold ${hover ? 'hover:text-gold-light' : '' } ${className} ${textSize}`}>
   <slot />
 </Link>
